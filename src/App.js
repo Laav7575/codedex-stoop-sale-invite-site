539a2f394e99b89f5f5c16057d3050b8a9d4b1df
lthiagal <lthiagal@uwaterloo.ca>
2024-07-11 21:44:15 -0400

    game added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import Map from "./components/map/Map.js";
-import { useJsApiLoader } from "@react-google-maps/api";
+import React from 'react';
+import { useJsApiLoader } from '@react-google-maps/api';
+import { RecoilRoot } from 'recoil';
+import Map from './components/map/Map.js';
+import Main from './components/main/main.jsx';
+import Game from './game/game.js';
+import { mapOptions } from './components/map/MapConfiguration.js';
 import './App.css';
-import Main from "./components/main/main.jsx";
-import { mapOptions } from "./components/map/MapConfiguration.js";
 
 function App() {
   const { isLoaded } = useJsApiLoader({
     id: mapOptions.googleMapApiKey,
-    googleMapsApiKey: mapOptions.googleMapApiKey
-  })
+    googleMapsApiKey: mapOptions.googleMapApiKey,
+  });
 
-  const googleMap = <Map isLoaded = {isLoaded} />
+  const googleMap = <Map isLoaded={isLoaded} />;
 
   return (
-    <div className="App">
-      <Main theMap={googleMap}/>
-    </div>
+    <RecoilRoot>
+      <Game />
+      <div className="App">
+        <Main theMap={googleMap} />
+      </div>
+    </RecoilRoot>
   );
-
 }
 
 export default App;
---
+++ src/game/score.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useRecoilValue} from 'recoil';
+import {scoreState} from './atom';
+
+const Score = () => {
+    const score = useRecoilValue(scoreState);
+    return (
+        <div className="score">
+            <p>{`Score: ${score}`}</p>
+        </div>
+    );
+};
+export default React.memo(Score);
