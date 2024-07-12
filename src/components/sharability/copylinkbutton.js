19b36b2bc5909a0fd101e50e0dee03801f630c94
lthiagal <lthiagal@uwaterloo.ca>
2024-07-12 09:24:25 -0400

    link for copy link updated

--- src/components/sharability/copylinkbutton.js
+++ src/components/sharability/copylinkbutton.js
@@ -3,8 +3,8 @@ import React from 'react';
 const CopyToClipboardButton = ({ content }) => {
   const handleCopy = async () => {
     try {
-      await navigator.clipboard.writeText("put link here");
-      alert('Copied!', "put link here");
+      await navigator.clipboard.writeText("https://codedex-super-stoop-sale.vercel.app/");
+      alert('Copied!', "https://codedex-super-stoop-sale.vercel.app/");
     } catch (error) {
       alert('Unable to copy to clipboard.. :(', error);
     }
